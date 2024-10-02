86e18b93589740a9dd0d091a584605216dbda666
Rohit Gade <155424199+Rohitgade14@users.noreply.github.com>
2024-10-02 11:56:03 +0530

    Add files via upload

---
+++ ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.sbi.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+	public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
